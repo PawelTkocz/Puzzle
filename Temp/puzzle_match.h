5960507dcbe61af1181cd3554326f62ebdb975fe
PawelTkocz <pawel.tkocz@vp.pl>
2023-09-25 00:59:07 +0200

    puzzle match dokladniej

--- Temp/puzzle_match.h
+++ Temp/puzzle_match.h
@@ -66,6 +66,9 @@ double get_height_points(struct PuzzleSide s1, struct PuzzleSide s2){
 }
 
 int get_side_comparisson_points(struct PuzzleSide *s1, struct PuzzleSide *s2){
+
+    int best_score = 100000;
+
     int is_print = 0;
     int width1 = s1->width;
     int width2 = s2->width;
@@ -81,132 +84,142 @@ int get_side_comparisson_points(struct PuzzleSide *s1, struct PuzzleSide *s2){
     int diff = longer->width - shorter->width;
     int margin = diff/2;
 
-    int height_above1 = s1->heighest;
-    int height_above2 = s2->heighest;
-    int max_height_above = height_above1;
-    if(height_above2>height_above1)
-        max_height_above = height_above2;
-
-    int height_under1 = s1->lowest;
-    int height_under2 = s2->lowest;
-    int min_height_under = height_under1;
-    if(height_under2<height_under1)
-        min_height_under = height_under2;
-
-    int bitmap[max_height_above-min_height_under+2][longer->width];
-    for(int i=0; i<max_height_above-min_height_under+2; i++)
-        for(int j=0; j<longer->width; j++)
-            bitmap[i][j] = 0;
-
-    int start_y = -1*min_height_under + 1;
-    int n1 = longer->points_num;
-    for(int i=0; i<n1; i++){
-        int x = longer->positions[i].x;
-        int y = longer->positions[i].y;
-        bitmap[start_y+y][x] = 2;
-    }
+    for(int v=0; v<21; v++){
+        int margin_y = v-10;
+        //wysokosc nad lub pod jaka zaczyna sie rysowanie boku krotszego w stosunku do dluzszego
 
 
-    if(is_print){
-        for(int i=max_height_above-min_height_under+1; i>=0; i--){
+        int height_above1 = longer->heighest;
+        int height_above2 = shorter->heighest + margin_y;
+        int max_height_above = height_above1;
+        if(height_above2>height_above1)
+            max_height_above = height_above2;
+
+        int height_under1 = longer->lowest;
+        int height_under2 = shorter->lowest + margin_y;
+        int min_height_under = height_under1;
+        if(height_under2<height_under1)
+            min_height_under = height_under2;
+
+        int bitmap[max_height_above-min_height_under+2][longer->width];
+        for(int i=0; i<max_height_above-min_height_under+2; i++)
             for(int j=0; j<longer->width; j++)
-                printf("%d", bitmap[i][j]);
-            printf("\n");
-        }
-        printf("\n");
-    }
+                bitmap[i][j] = 0;
 
-    int directions[4] = {0, 0, -1, 1};
-    struct Node *queue = NULL;
-    insert_node(&queue, 0, 0);
-    int first_cnt = 1;
-    bitmap[0][0] = 1;
-    while(queue != NULL){
-        int y = queue->y;
-        int x = queue->x;
-        delete_start(&queue);
-        for(int p=0; p<4; p++){
-            int i = directions[p];
-            int j = directions[3-p];
-            if(x+j<0 || x+j>=longer->width || y+i < 0 || y+i >= max_height_above-min_height_under+2)
-                continue;
-            if(bitmap[y+i][x+j]==0){
-                bitmap[y+i][x+j]=1;
-                first_cnt++;
-                insert_node(&queue, y+i, x+j);
+        int start_y = -1*min_height_under + 1;
+        int n1 = longer->points_num;
+        for(int i=0; i<n1; i++){
+            int x = longer->positions[i].x;
+            int y = longer->positions[i].y;
+            bitmap[start_y+y][x] = 2;
+        }
+
+
+        if(is_print){
+            for(int i=max_height_above-min_height_under+1; i>=0; i--){
+                for(int j=0; j<longer->width; j++)
+                    printf("%d", bitmap[i][j]);
+                printf("\n");
             }
-        }
-    }
-
-    if(is_print){
-        for(int i=max_height_above-min_height_under+1; i>=0; i--){
-            for(int j=0; j<longer->width; j++)
-                printf("%d", bitmap[i][j]);
             printf("\n");
         }
-        printf("\n");
-    }
 
-    //printf("first_cnt %d\n", first_cnt);
-
-    if(bitmap[max_height_above-min_height_under+1][0] == 1 || bitmap[max_height_above-min_height_under+1][longer->width-1] == 1)
-        printf("ZAPELNIONO CALA BITMAPE\n");
-
-    int n2 = shorter->points_num;
-    for(int i=0; i<n2; i++){
-        int x = shorter->positions[i].x;
-        int y = shorter->positions[i].y;
-        bitmap[start_y+y][margin+x] = 3;
-    }
-    for(int i=shorter->positions[0].y+start_y; i>=0; i--)
-        bitmap[i][margin+shorter->positions[0].x] = 3;
-    for(int i=shorter->positions[n2-1].y+start_y; i>=0; i--)
-        bitmap[i][margin+shorter->positions[n2-1].x] = 3;
-
-    insert_node(&queue, 0, (longer->width)/2);
-    int new_points = 0;
-    bitmap[0][(longer->width)/2] = 4;
-    first_cnt--;
-    while(queue != NULL){
-        int y = queue->y;
-        int x = queue->x;
-        delete_start(&queue);
-        for(int p=0; p<4; p++){
-            int i = directions[p];
-            int j = directions[3-p];
-            if(i==0 && j==0)
-                continue;
-            if(x+j<0 || x+j>=longer->width || y+i < 0 || y+i >= max_height_above-min_height_under+2)
-                continue;
-            if(bitmap[y+i][x+j]==0 || bitmap[y+i][x+j]==1 || bitmap[y+i][x+j]==2){
-                insert_node(&queue, y+i, x+j);
-                if(bitmap[y+i][x+j]==0 || bitmap[y+i][x+j]==2){
-                    new_points++;
-                    bitmap[y+i][x+j]=8;
-                }
-                else{
-                    first_cnt--;
-                    bitmap[y+i][x+j]=4;
+        int directions[4] = {0, 0, -1, 1};
+        struct Node *queue = NULL;
+        insert_node(&queue, 0, 0);
+        int first_cnt = 1;
+        bitmap[0][0] = 1;
+        while(queue != NULL){
+            int y = queue->y;
+            int x = queue->x;
+            delete_start(&queue);
+            for(int p=0; p<4; p++){
+                int i = directions[p];
+                int j = directions[3-p];
+                if(x+j<0 || x+j>=longer->width || y+i < 0 || y+i >= max_height_above-min_height_under+2)
+                    continue;
+                if(bitmap[y+i][x+j]==0){
+                    bitmap[y+i][x+j]=1;
+                    first_cnt++;
+                    insert_node(&queue, y+i, x+j);
                 }
             }
         }
-    }
-    //printf("new_points first_cnt %d %d\n", new_points, first_cnt);
 
-    if(is_print){
-        for(int i=max_height_above-min_height_under+1; i>=0; i--){
-            for(int j=0; j<longer->width; j++)
-                printf("%d", bitmap[i][j]);
+        if(is_print){
+            for(int i=max_height_above-min_height_under+1; i>=0; i--){
+                for(int j=0; j<longer->width; j++)
+                    printf("%d", bitmap[i][j]);
+                printf("\n");
+            }
             printf("\n");
         }
-        printf("\n");
+
+        //printf("first_cnt %d\n", first_cnt);
+
+        if(bitmap[max_height_above-min_height_under+1][0] == 1 || bitmap[max_height_above-min_height_under+1][longer->width-1] == 1)
+            printf("ZAPELNIONO CALA BITMAPE\n");
+
+        int n2 = shorter->points_num;
+        for(int i=0; i<n2; i++){
+            int x = shorter->positions[i].x;
+            int y = shorter->positions[i].y;
+            bitmap[start_y+margin_y+y][margin+x] = 3;
+        }
+        for(int i=shorter->positions[0].y+start_y+margin_y; i>=0; i--)
+            bitmap[i][margin+shorter->positions[0].x] = 3;
+        for(int i=shorter->positions[n2-1].y+start_y+margin_y; i>=0; i--)
+            bitmap[i][margin+shorter->positions[n2-1].x] = 3;
+
+        insert_node(&queue, 0, (longer->width)/2);
+        int new_points = 0;
+        bitmap[0][(longer->width)/2] = 4;
+        first_cnt--;
+        while(queue != NULL){
+            int y = queue->y;
+            int x = queue->x;
+            delete_start(&queue);
+            for(int p=0; p<4; p++){
+                int i = directions[p];
+                int j = directions[3-p];
+                if(i==0 && j==0)
+                    continue;
+                if(x+j<0 || x+j>=longer->width || y+i < 0 || y+i >= max_height_above-min_height_under+2)
+                    continue;
+                if(bitmap[y+i][x+j]==0 || bitmap[y+i][x+j]==1 || bitmap[y+i][x+j]==2){
+                    insert_node(&queue, y+i, x+j);
+                    if(bitmap[y+i][x+j]==0 || bitmap[y+i][x+j]==2){
+                        new_points++;
+                        bitmap[y+i][x+j]=8;
+                    }
+                    else{
+                        first_cnt--;
+                        bitmap[y+i][x+j]=4;
+                    }
+                }
+            }
+        }
+        //printf("new_points first_cnt %d %d\n", new_points, first_cnt);
+
+        if(is_print){
+            for(int i=max_height_above-min_height_under+1; i>=0; i--){
+                for(int j=0; j<longer->width; j++)
+                    printf("%d", bitmap[i][j]);
+                printf("\n");
+            }
+            printf("\n");
+        }
+
+
+        if(bitmap[max_height_above-min_height_under+1][0] == 4 || bitmap[max_height_above-min_height_under+1][longer->width-1] == 4)
+            printf("DRUGI ZAPELNIL NAJPRAWDOPODOBNIEJ CALA BITMAPE\n");
+
+        int res = first_cnt+new_points;
+        if(res < best_score)
+            best_score = res;
     }
 
-
-    if(bitmap[max_height_above-min_height_under+1][0] == 4 || bitmap[max_height_above-min_height_under+1][longer->width-1] == 4)
-        printf("DRUGI ZAPELNIL NAJPRAWDOPODOBNIEJ CALA BITMAPE\n");
-
-    return first_cnt+new_points;
+    return best_score;
 }
 
 double puzzle_match_val(struct PuzzleSide *s1, struct PuzzleSide *s2){
